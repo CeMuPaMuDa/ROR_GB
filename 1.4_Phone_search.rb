7ae7dd9ab1dd4571d26198c45f96fbe72a2ed83f
CeMuPaMuDa <bepyc9i@gmail.com>
2021-11-25 12:42:17 +0200

    4 complate

---
+++ 1.4_Phone_search.rb
@@ -0,0 +1,12 @@
+require 'html_to_plain_text'
+require 'faraday'
+url = 'https://gb.ru'
+response = Faraday.get(url)
+html = response.body
+arr =  HtmlToPlainText.plain_text(html).lines(chomp: true)
+phone = []
+arr.each do |num|
+  phone.push num if num.match(/^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/)
+end
+puts "Телефоны тех.поддержки GeekBrains: "
+puts phone.uniq
