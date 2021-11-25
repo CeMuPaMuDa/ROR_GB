ccdb2b72f8ec9494d3c9d65504303a288b20eb49
CeMuPaMuDa <bepyc9i@gmail.com>
2021-11-25 13:45:21 +0200

    fix & rubocop

--- 1.4_Phone_search.rb
+++ 1.4_Phone_search.rb
@@ -1,12 +1,11 @@
+# frozen_string_literal: true
+
 require 'html_to_plain_text'
 require 'faraday'
 url = 'https://gb.ru'
 response = Faraday.get(url)
 html = response.body
-arr =  HtmlToPlainText.plain_text(html).lines(chomp: true)
-phones = []
-arr.each do |num|
-  phone.push num if num.match(/^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/)
-end
-puts "Телефоны тех.поддержки GeekBrains: "
-puts phones.uniq
+puts 'Телефоны тех.поддержки GeekBrains: '
+puts HtmlToPlainText.plain_text(html).lines(chomp: true)
+                    .select { |num| num.match(/^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/) }
+                    .uniq
