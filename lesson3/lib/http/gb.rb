89c12cfa43e0192a8a8578d5a0f2d9905a09ca95
CeMuPaMuDa <bepyc9i@gmail.com>
2021-12-06 23:34:12 +0200

    rubocop

--- lesson3/lib/http/gb.rb
+++ lesson3/lib/http/gb.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class Gb
-def self.count(url = 'https://gb.ru/courses')
-  Faraday.get(url).body.lines(chomp: true).count
-end
+  def self.count(url = 'https://gb.ru/courses')
+    Faraday.get(url).body.lines(chomp: true).count
+  end
 end
